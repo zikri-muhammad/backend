70ca9a364dba214ead9cb9fed321c1a9d6dcbfff
zikri-muhammad <muhammadzikri598@gmail.com>
2023-06-22 17:10:55 +0700

    routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response, NextFunction } from "express";
 
 const app: Application = express();
 
+
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   console.log("My Frist commit");
   res.send("hello")
